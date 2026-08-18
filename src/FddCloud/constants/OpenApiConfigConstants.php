f1888af841892dc7cce4d7b5975115efb9cfe1f2
yeuyeduyin <36926660+yeuyeduyin@users.noreply.github.com>
2026-08-18 11:21:56 +0800

    Modify OpenApiConfigConstants for new credentials
    Updated SERVICE_URL, APP_ID, and APP_SECRET constants for configuration.

--- src/FddCloud/constants/OpenApiConfigConstants.php
+++ src/FddCloud/constants/OpenApiConfigConstants.php
@@ -13,24 +13,16 @@ class OpenApiConfigConstants
      */
     const SERVICE_URL = "https://uat-api.fadada.com/api/v5/";
 
-//    const SERVICE_URL = "https://sit-api.fadada.com/api/v5/";
-//    const SERVICE_URL = "http://127.0.0.1:8013/api/v5/v2"; //本地
-
     /**
      * 应用appid
      */
-    const APP_ID = "00000199";
-//    const APP_ID = "00000385"; //sit
-//    const APP_ID = "00000535";
+    const APP_ID = "00000xxx";
 
     /**
      * 应用秘钥
      */
-    const APP_SECRET = "JTYPDTNO2J9N0KLY6ZWBQ7SJUKGBHYSK";
+    const APP_SECRET = "JTYPDTNO2J9Nxxxxxxxxxxxxxxxx";
 
-//    const APP_SECRET = "5V8D3QQLCTUEEWE8VAH5K2UNXACXF9KQ"; //sit
-
-//    const APP_SECRET = "KFUQIW6IOKDMJT1XGEFGBF1GDZI8TSTM";
 
     const DEBUG = false;
 }
